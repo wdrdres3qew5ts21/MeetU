3cdd134c604cab2208492ec4e221468325816140
Unknown <supakorn.trakulmaiphol@mail.kmutt.ac.th>
2019-03-16 23:14:43 +0700

    [ADD] EurekaClient Event, User Service

--- Microservice/UserService/src/main/java/meetu/userservice/UserServiceApplication.java
+++ Microservice/UserService/src/main/java/meetu/userservice/UserServiceApplication.java
@@ -2,7 +2,9 @@ package meetu.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
 
