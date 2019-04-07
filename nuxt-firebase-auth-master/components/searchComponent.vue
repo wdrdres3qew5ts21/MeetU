a0bd607f9b9774e7b9ea255d7d9c2505a30c85e6
kanwara <fonfonvfc@gmail.com>
2019-04-07 20:54:58 +0700

    [ADD,EDIT]FE หน้า search แก้ไขจาก commit ที่ลบไป

--- nuxt-firebase-auth-master/components/searchComponent.vue
+++ nuxt-firebase-auth-master/components/searchComponent.vue
@@ -13,19 +13,7 @@
         prepend-icon="search"
         return-object
       ></v-autocomplete>
-    <!-- <v-expand-transition>
-      <v-list v-if="model" class="red lighten-3">
-        <v-list-tile
-          v-for="(field, i) in fields"
-          :key="i"
-        >
-          <v-list-tile-content>
-            <v-list-tile-title v-text="field.value"></v-list-tile-title>
-            <v-list-tile-sub-title v-text="field.key"></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-expand-transition> -->
+
 </template>
 
 
---
+++ nuxt-firebase-auth-master/pages/search.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <search-component></search-component>
+            
+
+  </v-container>
+</template>
+
+<script>
+import searchComponent from '~/components/searchComponent'
+
+export default {
+  components: {
+    searchComponent,
+
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
