26b5978aaca7c3ffecd0ea1e3e24fb16573960cd
kanwara <fonfonvfc@gmail.com>
2019-05-08 01:00:04 +0700

    [FE]add หน้า userProfile.vue

---
+++ nuxt-firebase-auth-master/pages/userProfile.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <user-profile-form></user-profile-form>
+  </v-container>
+</template>
+
+<script>
+import userProfileForm from '~/components/userProfileForm'
+
+
+export default {
+  components: {
+    userProfileForm,
+  }
+}
+</script>
+
+<style lang="css">
+</style>
