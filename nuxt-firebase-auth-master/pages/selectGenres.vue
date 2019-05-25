4bd66a9ac865d4d91ad8646c9beaec2b4a64b028
kanwara <fonfonvfc@gmail.com>
2019-05-25 10:00:49 +0700

    [ADD]FE selectGenres

---
+++ nuxt-firebase-auth-master/pages/selectGenres.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <genres></genres>
+    <h3>Choose genres you like
+so we can suggest to follow</h3>
+  </v-container>
+</template>
+
+<script>
+import genres from '~/components/genres'
+
+
+export default {
+  components: {
+    genres,
+  }
+}
+</script>
+
+<style lang="css">
+</style>
