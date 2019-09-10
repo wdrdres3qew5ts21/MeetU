97ac3b7ce3209feab630f96c009c4d67696d4e94
virada <virada.weera@mail.kmutt.ac.th>
2019-09-10 16:50:17 +0700

    [UPDATE] FE หน้า Use profile form

---
+++ nuxt-firebase-auth-master/pages/community/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-text>
+        <span class="text--primary">
+          <span>Community name :</span>
+          <br />
+          <span>Members :</span>
+          <br />
+        </span>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="white--text" color="#341646">JOIN</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+<style>
+</style>
