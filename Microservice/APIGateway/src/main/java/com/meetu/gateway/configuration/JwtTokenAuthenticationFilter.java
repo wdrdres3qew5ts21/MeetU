858c411234f795360f7ea563d0b2f5193fbaa88d
LinJingYun <wdrdres3qew5ts21@gmail.com>
2019-09-29 02:11:27 +0700

    [ADD] API Gateway ระบบ Validate ด้วย Spring Security

---
+++ Microservice/APIGateway/src/main/java/com/meetu/gateway/configuration/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,81 @@
+package com.meetu.gateway.configuration;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Role;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Authenticate requests with header 'Authorization: Bearer jwt-token'.
+ *
+ * @author raj
+ */
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationConfig config;
+
+    public JwtTokenAuthenticationFilter(JwtAuthenticationConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse rsp, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = req.getHeader(config.getHeader());
+        System.out.println("fuq filter");
+        System.out.println(token);
+        if (token != null && token.startsWith(config.getPrefix() + " ")) {
+            try {
+                token = token.replace(config.getPrefix() + " ", "");
+                System.out.println(token);
+                FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                String uid = decodedToken.getUid();
+                String email = decodedToken.getEmail();
+                List<String> authorities = new ArrayList<>();
+                authorities.add("user");
+                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, "jwt", authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                System.out.println("Authenicated : " + SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
+                System.out.println(SecurityContextHolder.getContext().getAuthentication());
+
+//            try {
+//                Claims claims = Jwts.parser()
+//                        .setSigningKey(config.getSecret().getBytes())
+//                        .parseClaimsJws(token)
+//                        .getBody();
+//                String username = claims.getSubject();
+//                @SuppressWarnings("unchecked")
+//                List<String> authorities = claims.get("authorities", List.class);
+//                if (username != null) {
+//                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null,
+//                            authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+//                    SecurityContextHolder.getContext().setAuthentication(auth);
+//                }
+//            } catch (Exception ignore) {
+//                SecurityContextHolder.clearContext();
+//            }
+            } catch (FirebaseAuthException ex) {
+                SecurityContextHolder.clearContext();
+                Logger.getLogger(JwtTokenAuthenticationFilter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        filterChain.doFilter(req, rsp);
+    }
+}
