c0ab245ba4c8f56660ff7a538234fd7eb9f4d83a
virada <virada.weera@mail.kmutt.ac.th>
2019-09-29 01:41:09 +0700

    [ADD] FE confirm ticket component

---
+++ nuxt-firebase-auth-master/components/confirmTicket.vue
@@ -0,0 +1,23 @@
+<template>
+  <div justify-center>
+    <center>
+      <v-icon size="100px" color="success">check_circle</v-icon>
+    </center>
+    <center>
+      <h1>THANK YOU!</h1>
+    </center>
+    <center>
+      <h3>Your order is complete</h3>
+    </center>
+    <br />
+    <h2>Event name:</h2>
+   
+      <v-btn block color="#341646" style="color:white" >View My Ticket</v-btn>
+   
+  </div>
+</template>
+<script>
+export default {
+  name: "confirmTicket"
+};
+</script>
