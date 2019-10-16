3bd06cb8cf72191563dfa79a06a5c450b2f91a2d
virada <virada.weera@mail.kmutt.ac.th>
2019-10-16 21:36:09 +0700

    [ADD] FE comment in each post

--- nuxt-firebase-auth-master/components/communityDetail.vue
+++ nuxt-firebase-auth-master/components/communityDetail.vue
@@ -55,128 +55,138 @@
 
     <br />
 
-    <v-card rounded outlined v-for="(todo,index ) in postList " :key="index">
-      <br />
-      <div>
-        <v-layout>
-          <v-container grid-list-xs fluid style="padding:10px">
-            <v-flex xs12 class="text-xs-left">
-              <v-avatar size="60">
-                <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
-              </v-avatar>
-              {{ getUser.displayName}}
-            </v-flex>
-          </v-container>
-          <v-flex xs12 class="text-xs-right">
-            <v-btn text icon>
-              <v-icon>expand_more</v-icon>
-            </v-btn>
-            <!-- <v-overflow-btn icon :items="remove">
-              <v-icon>expand_more</v-icon>
-                 @click="removePost(todo)"
-                  color="#341646"
-                  class="mb-2 white--text"
-                  type="button"
-                  name="button"
-            </v-overflow-btn>-->
-          </v-flex>
-        </v-layout>
-      </div>
-      <v-container grid-list-xs fluid style="padding:10px">
+    <div v-for="(todo,postIndex ) in postList " :key="postIndex">
+      <v-card rounded outlined>
         <br />
-        <v-layout wrap>
-          <span :class="{ done: todo.done }">{{todo.post}}</span>
-        </v-layout>
-      </v-container>
-      <v-card-text rounded outlined class="mx-auto">
-        <v-divider></v-divider>
-        <v-flex class="text-right">
-          <v-layout wrap justify-end>
-            <v-dialog
-              v-model="dialog"
-              fullscreen
-              hide-overlay
-              transition="dialog-bottom-transition"
-            >
-              <template v-slot:activator="{ on }">
-                <v-btn v-on="on" text block flat>
-                  <v-icon>comment</v-icon>Comment
-                </v-btn>
-              </template>
-              <v-card>
-                <v-toolbar dark color="primary">
-                  <v-btn icon dark @click="dialog = false">
-                    <v-icon>close</v-icon>
-                  </v-btn>
-                  <v-toolbar-title>comment</v-toolbar-title>
-                </v-toolbar>
-                <v-card rounded outlined v-for="(todo,index ) in commentList " :key="index">
-                  <v-container grid-list-xs fluid style="padding:10px">
-                    <v-flex xs12 class="text-xs-left">
-                      <v-avatar size="30">
-                        <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
-                      </v-avatar>
-                      {{ getUser.displayName}}
-                      <br />
-                      <span></span>
-                      <span :class="{ done: todo.done }">{{todo.commentInPost}}</span>
-                    </v-flex>
-                  </v-container>
-
-                  <br />
-                </v-card>
-                <form >
-                  <v-layout ma-3>
-                    <v-flex xs12>
-                      <v-flex lass="text-xs-left">
-                        <v-text-field
-                          v-model="newComment"
-                          name="newComment"
-                          id="newComment"
-                          value
-                          label="write something..."
-                        ></v-text-field>
-                      </v-flex>
-                    </v-flex>
-                    <v-flex class="text-xs-right" pa-2>
-                      <v-btn text small @click="addComment()">post</v-btn>
-                    </v-flex>
-                  </v-layout>
-                </form>
-              </v-card>
-            </v-dialog>
-
-            <!-- <v-btn
+        <div>
+          <v-layout>
+            <v-container grid-list-xs fluid style="padding:10px">
+              <v-flex xs12 class="text-xs-left">
+                <v-avatar size="60">
+                  <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
+                </v-avatar>
+                {{ getUser.displayName}}
+              </v-flex>
+            </v-container>
+            <v-flex xs12 class="text-xs-right">
+              <v-btn text icon>
+                <v-icon>expand_more</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </div>
+        <v-container grid-list-xs fluid style="padding:10px">
+          <br />
+          <v-layout wrap>
+            <span :class="{ done: todo.done }">{{todo.post}}</span>
+            <h1>{{postIndex}}</h1>
+          </v-layout>
+        </v-container>
+        <v-card-text rounded outlined class="mx-auto">
+          <v-divider></v-divider>
+          <v-flex class="text-right">
+            <v-btn text block flat @click="getCommentFromPost(postIndex)">
+              <v-icon>comment</v-icon>Comment
+            </v-btn>
+            <v-layout wrap justify-end>
+              <!-- <v-btn
                   @click="removePost(todo)"
                   color="#341646"
                   class="mb-2 white--text"
                   type="button"
                   name="button"
-            >Remove</v-btn>-->
-          </v-layout>
-        </v-flex>
-        <v-divider></v-divider>
-        <v-layout>
-          <br />
-        </v-layout>
-      </v-card-text>
-
-      <v-card rounded outlined v-for="(todo,index ) in commentList " :key="index">
-        <v-container grid-list-xs fluid style="padding:10px">
-          <v-flex xs12 class="text-xs-left">
-            <v-avatar size="30">
-              <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
-            </v-avatar>
-            {{ getUser.displayName}}
-            <br />
-            <span></span>
-            <span :class="{ done: todo.done }">{{todo.commentInPost}}</span>
+              >Remove</v-btn>-->
+            </v-layout>
           </v-flex>
-        </v-container>
+          <v-divider></v-divider>
+          <v-layout>
+            <br />
+          </v-layout>
+        </v-card-text>
 
+        <v-card
+          rounded
+          outlined
+          v-for="(comment,commentIndex ) in postList[postIndex].commentList "
+          :key="commentIndex"
+        >
+          <v-container grid-list-xs fluid style="padding:10px">
+            <v-flex xs12 class="text-xs-left">
+              <v-avatar size="30">
+                <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
+              </v-avatar>
+              {{ getUser.displayName}}
+              <br />
+              <span></span>
+              <span :class="{ done: todo.done }">{{comment}}</span>
+            </v-flex>
+          </v-container>
+
+          <br />
+        </v-card>
+        <br />
         <br />
       </v-card>
-    </v-card>
+      <br />
+      <br />
+    </div>
+
+    <div v-if="postList.length != 0">
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <!-- <template v-slot:activator="{ on }">
+                <v-btn v-on="on" text block flat @click="getCommentFromPost(postIndex)">
+                  <v-icon>comment</v-icon>Comment
+                </v-btn>
+        </template>-->
+        <v-card>
+          <v-toolbar dark color="primary">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>close</v-icon>
+            </v-btn>
+            <v-toolbar-title>comment</v-toolbar-title>
+          </v-toolbar>
+
+          <v-card
+            rounded
+            outlined
+            v-for="(comment,commentIndex ) in  postList[postIndex].commentList "
+            :key="commentIndex"
+          >
+            <v-container grid-list-xs fluid style="padding:10px">
+              <v-flex xs12 class="text-xs-left">
+                <v-avatar size="30">
+                  <v-img :aspect-ratio="1/1" :src="getUser.photoURL"></v-img>
+                </v-avatar>
+                {{ getUser.displayName}}
+                <br />
+                <span></span>
+                <span>{{comment}}</span>
+                <span>{{postList}}</span>
+              </v-flex>
+            </v-container>
+          </v-card>
+          <form>
+            {{postIndex}}
+            <v-layout ma-3>
+              <v-flex xs12>
+                <v-flex lass="text-xs-left">
+                  <v-text-field
+                    v-model="comment"
+                    name="newComment"
+                    id="newComment"
+                    value
+                    label="write something..."
+                  ></v-text-field>
+                </v-flex>
+              </v-flex>
+              <v-flex class="text-xs-right" pa-2>
+                <v-btn text small @click="addComment(postIndex)">Comment</v-btn>
+              </v-flex>
+            </v-layout>
+          </form>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template> 
  
@@ -196,9 +206,11 @@ export default {
       post: "",
       newPost: "",
       postList: [],
+      //postList: [{post: '' , commentList:[]}],
       commentInPost: "",
-      newComment: "",
-      commentList: [],
+      comment: "",
+      // newcomment: [],
+      postIndex: 0,
       dialog: false,
       name: "",
       rules: [
@@ -206,7 +218,7 @@ export default {
           !value ||
           value.size < 2000000 ||
           "Avatar size should be less than 2 MB!"
-      ]
+      ],
     };
   },
   components: {
@@ -243,6 +255,15 @@ export default {
       fileReader.readAsDataURL(files[0]);
       this.image = files[0];
     },
+    getCommentFromPost(postIndex) {
+      this.dialog = true;
+      this.postIndex = postIndex
+      console.log(
+        "--------------------postIndex get comment from post ----------------------"
+      );
+      console.log(postIndex);
+      console.log(this.postList[postIndex].commentList);
+    },
 
     addPost() {
       this.postList.push({
@@ -250,24 +271,45 @@ export default {
         done: false
       });
       this.newPost = "";
+      console.log(this.postList);
     },
-    removePost(todo) {
-      const postIndex = this.postList.indexOf(todo);
-      this.postList.splice(postIndex, 1);
-    },
+    // removePost(todo) {
+    //   const postIndex = this.postList.indexOf(todo);
+    //   this.postList.splice(postIndex, 1);
+    // },
 
-    addComment() {
-      this.commentList.push({
-        commentInPost: this.newComment,
-        done: false
-      });
-      this.newComment = "";
+    addComment(postIndex) {
+      console.log("------ ADD Comment ----");
+      console.log(postIndex);
+      console.log("-------- Post List ------")
+      console.log(this.postList)
+      console.log("------------------")
+      this.postIndex = postIndex;
+      let isCommentList = this.postList[postIndex].commentList;
+      console.log(isCommentList);
+
+      if (isCommentList === undefined) {
+        console.log("It's first comment");
+        this.postList[postIndex].commentList = [];
+      }
+
+      this.postList[postIndex].commentList.push(this.comment);
+      console.log(this.postList[postIndex].commentList);
+      this.comment = "";
       this.dialog = false;
-    },
-    removePost(todo) {
-      const commentIndex = this.commentList.indexOf(todo);
-      this.commentList.splice(commentIndex, 1);
+      console.log(postIndex);
     }
+    // addComment() {
+    //     this.postList.commentList.push({
+    //     // commentList: this.commentList.push({
+    //     comment:  this.newComment,
+    //     done: false
+    //     // }),
+
+    //   });
+    //   this.newComment = "";
+    //   this.dialog = false;
+    // },
   }
 };
 </script> 
