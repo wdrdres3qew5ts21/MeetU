89bb82c4904667ca19799f39333d6d229f1e60fd
virada <virada.weera@mail.kmutt.ac.th>
2019-10-18 23:58:06 +0700

    [ADD] FE Badge

--- nuxt-firebase-auth-master/pages/ranking/badge.vue
+++ nuxt-firebase-auth-master/pages/ranking/badge.vue
@@ -1,5 +1,145 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <center>
+      <h2>Ranking Board</h2>
+    </center>
+    <v-form>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-text-field
+              v-model="message"
+              :append-outer-icon="message ? 'search' : 'search'"
+              :prepend-icon="icon"
+              box
+              clear-icon="close"
+              clearable
+              label="Message"
+              type="text"
+              @click:append="toggleMarker"
+              @click:append-outer="sendMessage"
+              @click:prepend="changeIcon"
+              @click:clear="clearMessage"
+            ></v-text-field>
+          </v-flex>
+          <br />
+        </v-layout>
+      </v-container>
+    </v-form>
+     <center>
+         <h2 >
+           Top badge 
+         </h2>
+          </center>
+          <br>
+          <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+      
+        <v-list three-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+            >
+              {{ item.header }}
+            </v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              avatar
+              @click=""
+            >
+              <v-list-tile-avatar>
+                <img :src="item.avatar">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </div>
+  
 </template>
+
+<script>
+export default {
+  data: () => ({
+    password: "Password",
+    show: false,
+    message: "",
+    marker: true,
+    iconIndex: 0,
+    icons: ["filter_list"],
+     items: [
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+            title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+            title: 'Oui oui',
+            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+            title: 'Birthday gift',
+            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+            title: 'Recipe to try',
+            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+          }
+        ]
+  }),
+
+  computed: {
+    icon() {
+      return this.icons[this.iconIndex];
+    }
+  },
+
+  methods: {
+    toggleMarker() {
+      this.marker = !this.marker;
+    },
+    sendMessage() {
+      this.resetIcon();
+      this.clearMessage();
+    },
+    clearMessage() {
+      this.message = "";
+    },
+    resetIcon() {
+      this.iconIndex = 0;
+    },
+    changeIcon() {
+      this.iconIndex === this.icons.length - 1
+        ? (this.iconIndex = 0)
+        : this.iconIndex++;
+    }
+  }
+};
+</script>
