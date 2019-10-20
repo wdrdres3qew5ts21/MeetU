9183c0e14d59cc8f8a9aea10643eff80a53947aa
virada <virada.weera@mail.kmutt.ac.th>
2019-10-20 22:21:52 +0700

    [ADD] FE เพิ่มหน้า my badge

--- nuxt-firebase-auth-master/pages/ranking/badge/myBadge.vue
+++ nuxt-firebase-auth-master/pages/ranking/badge/myBadge.vue
@@ -1,10 +1,79 @@
 <template>
+  <div>
     <div>
-        <h1>My Badge</h1>
+      <br />
+      <center>
+    <h1>My Badges</h1>
+      </center>
+      <br />
     </div>
+
+   
+  
+    <v-data-table
+      :items="badges"
+      :pagination.sync="pagination"
+      item-key="level"
+      class="elevation-1"
+    >
+    <template v-slot:no-data>
+    <v-alert :value="true" color="pink" icon="info"
+    >
+    <center>
+    Your badge not found ! <br>
+    Let's join event for get badge
+    </center>
+    </v-alert>
+    </template>
+
+      <template v-slot:items="props">
+        <tr>
+          <!-- <td>{{badges.index}} </td> -->
+          <td>
+            <br />
+            <center>
+              <v-avatar size="60">
+                <img :src="props.item.avatar" />
+              </v-avatar>
+            </center>
+            <br />
+          </td>
+          <td>
+            <h2>{{ props.item.name }}</h2>
+            <br />Detail:
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
 </template>
+
 <script>
 export default {
-    
-}
+  data: () => ({
+    pagination: {
+      sortBy: "level",
+      descending: true
+    },
+    badges: [
+      {
+        name: "Frozen Yogurt",
+        level: 1,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"
+      },
+      {
+        name: "Ice cream sandwich",
+        level: 2,
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"
+      }
+    ]
+  }),
+  
+};
 </script>
+
+<style >
+h1 {
+  color: #341646;
+}
+</style>
