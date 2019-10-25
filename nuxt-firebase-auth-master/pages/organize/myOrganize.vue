c3d29f569dac5107512499c25688ebda04b0d889
virada <virada.weera@mail.kmutt.ac.th>
2019-10-25 13:39:30 +0700

    [FIX] FE แก้ตัวแปร tabModel

--- nuxt-firebase-auth-master/pages/organize/myOrganize.vue
+++ nuxt-firebase-auth-master/pages/organize/myOrganize.vue
@@ -107,7 +107,7 @@ export default {
   data() {
     return {
       organizeList: null,
-      model: "tabModel",
+      tabModel: "tabModel",
       tabTopics: ["Organize", "Review", "Edit"],
       text:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
