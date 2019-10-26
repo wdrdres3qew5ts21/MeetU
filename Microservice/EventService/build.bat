b1e4478b24bc9b4ff7fd2ed96ca69db20eab6c9b
LinJingYun <wdrdres3qew5ts21@gmail.com>
2019-10-26 19:19:45 +0700

    [FIX] UserService & EventService แก้ Script คำสั่ง build ที่พิมพ์ค่าผิด

--- Microservice/EventService/build.bat
+++ Microservice/EventService/build.bat
@@ -1,1 +1,1 @@
-mvn clean package &&docker build -t linxianer12/eventservice:latest . &&docker push linxianer12/eventservicelinxianer12/eventservice:latest
+mvn clean package &&docker build -t linxianer12/eventservice:latest . && docker push linxianer12/eventservice:latest
--- Microservice/UserService/build.bat
+++ Microservice/UserService/build.bat
@@ -1,1 +1,1 @@
-mvn clean package && docker build -t linxianer12/userservice:latest . && docker push  linxianer12/userservice
+mvn clean package && docker build -t linxianer12/userservice:latest . && docker push  linxianer12/userservice:latest
