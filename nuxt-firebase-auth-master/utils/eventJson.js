2e9fcb8a0f4b34afbaee76ca8e2704aae8ad0e6b
LinJingYun <wdrdres3qew5ts21@gmail.com>
2019-11-03 15:36:14 +0700

    [FIX] FE แสดงภาพ Event Cover ขึ้นเป้นภาพแรกเมื่อเข้าไปในกิจกรรมนั้นๆ

--- nuxt-firebase-auth-master/utils/eventJson.js
+++ nuxt-firebase-auth-master/utils/eventJson.js
@@ -1,20 +1,13 @@
 export const mockCarouselsPhoto = [
     {
         src:
-            "https://www.rabbittoday.com/-/media/rabbittoday/content/events/bangkok-block-party-2019/bangkok-block-party-2019-event-banner.jpg?la=th-TH&hash=FE90C817F1C953E5DCB6834242EE517801037D28"
+            "https://inwfile.com/s-cf/65ro4x.jpg"
     },
-    {
-        src:
-            "https://www.rabbittoday.com/-/media/rabbittoday/content/events/atlantiswaterfestival2019/atlantiswaterfestival2019-banner.jpg?la=th-TH&hash=5346DE429E1704AF25E6CCBC7FDC25CA56AF09ADg"
-    },
-    {
-        src:
-            "https://www.rabbittoday.com/-/media/rabbittoday/content/events/ed-sheeran-divide-world-tour-2019/ed-sheeran-divide-world-tour-2019-event-rabbit-today-banner.jpg?la=th-TH&hash=C6B48BA5F7F79606DCE4B88DDE9A51EE9FE25A1C"
-    },
-    {
-        src:
-            "https://www.rabbittoday.com/-/media/rabbittoday/content/events/siamsongkranmusicfestival/siamsongkranmusicfestival-banner.jpg?la=th-TH&hash=6DBCBED42B97294F2E6AAE635F84BCE455518957"
-    }
+    // {
+    //     src:
+    //         "https://www.rabbittoday.com/-/media/rabbittoday/content/events/atlantiswaterfestival2019/atlantiswaterfestival2019-banner.jpg?la=th-TH&hash=5346DE429E1704AF25E6CCBC7FDC25CA56AF09ADg"
+    // },
+  
 ]
 
 export const mockPopularEventList = [
@@ -32,20 +25,20 @@ export const mockPopularEventList = [
             "https://i.pinimg.com/originals/0f/04/6f/0f046f654320d387d7608a754f205d1e.png",
         eventDate: new Date(1554653418)
     },
-    {
-        eventId: "11114a",
-        eventName: "Mars Event",
-        eventPictureCover:
-            "https://s3.amazonaws.com/thumbnails.venngage.com/template/70fdebc4-7bb7-49fc-9caf-700f890de92b.png",
-        eventDate: new Date(1554653418)
-    },
-    {
-        eventId: "11115a",
-        eventName: "Night Market",
-        eventPictureCover:
-            "https://stumbler.co.za/wp-content/uploads/2017/11/26685184_140352653308218_9209494944451264947_o-4.jpg",
-        eventDate: new Date(1554653418)
-    }
+    // {
+    //     eventId: "11114a",
+    //     eventName: "Mars Event",
+    //     eventPictureCover:
+    //         "https://s3.amazonaws.com/thumbnails.venngage.com/template/70fdebc4-7bb7-49fc-9caf-700f890de92b.png",
+    //     eventDate: new Date(1554653418)
+    // },
+    // {
+    //     eventId: "11115a",
+    //     eventName: "Night Market",
+    //     eventPictureCover:
+    //         "https://stumbler.co.za/wp-content/uploads/2017/11/26685184_140352653308218_9209494944451264947_o-4.jpg",
+    //     eventDate: new Date(1554653418)
+    // }
 ]
 
 export const mockCommunityList = [
