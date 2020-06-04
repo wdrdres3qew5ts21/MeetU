e47cd4cd89e6211059edac75b15d632717a0bc82
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-06-04 11:10:41 +0700

    [ADD] Elasticsearch TLS เฉพาะ Cluster ที่ Transport Layer ส่วน Traffic ข้างนอกใช้ HTTP

---
+++ changePassword.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+curl -X POST -v -k --cacert /usr/share/elasticsearch/config/certificates/ca/ca.crt "https://elasticsearch1:9200/_security/user/kibana/_password?pretty"  -u elastic:PleaseChangeMe   -H 'Content-Type: application/json' -d' { "password" : "PleaseChangeMe" } '
+
+if [[ $? == 51 ]]
+then
+  echo "Connect but it not secure So fail to change Password"
+else
+  echo "Skip SSL Secure Connection by -k argument so Change password Success"
+fi
+
+echo "======="
+echo "Variable two sign : $$?"
+echo "Variable single sign: $?"
+
+#!/bin/bash
+
+curl -k https://elasticsearch1:9200
+
+echo "Yeah $?"
