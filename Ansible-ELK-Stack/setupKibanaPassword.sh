9a96f487f651a6ea3a836bf62b1b7054669f2603
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-06-07 23:48:37 +0700

    [FIX] ansible module uri setup password ให้กับ kibana ผ่าน role elk_ssl

--- Ansible-ELK-Stack/setupKibanaPassword.sh
+++ Ansible-ELK-Stack/setupKibanaPassword.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+# while true
+# do
+#   curl  --fail -k -u elastic:PleaseChangeMe \
+#     "https://localhost:9200/_cluster/health?wait_for_status=yellow" &&  break
+#   sleep 5
+# done
+
 while true
 do
-  curl  --fail -u elastic:PleaseChangeMe \
-    "http://localhost:9200/_cluster/health?wait_for_status=yellow" \
-    && break
+  curl  --fail -k -u elastic:PleaseChangeMe \
+    curl -X POST -v -k  "https://localhost:9200/_security/user/kibana/_password?pretty"  -u elastic:PleaseChangeMe   -H 'Content-Type: application/json' -d ' { "password" : "PleaseChangeMe" } ' &&  break
   sleep 5
 done
 
-curl -X POST -v -k "http://localhost:9200/_security/user/kibana/_password?pretty" -u elastic:PleaseChangeMe   -H 'Content-Type: application/json' -d ' { "password" : "PleaseChangeMe" } '  && echo "success change password success"
-    
-  
+echo "fuckkkkkkkkkkkkkkkkkkk"
+curl -X POST -v -k  "https://localhost:9200/_security/user/kibana/_password?pretty"  -u elastic:PleaseChangeMe   -H 'Content-Type: application/json' -d ' { "password" : "PleaseChangeMe" } '
