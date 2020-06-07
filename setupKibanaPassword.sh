cbb9ea8369c02e216e058e732df967648ec4229b
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-06-07 21:25:01 +0700

    [FIX] shell script ตั้ง password kibana

--- setupKibanaPassword.sh
+++ setupKibanaPassword.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 while true
 do
-  curl  --fail -u "elastic:" \
+  curl  --fail -u elastic:PleaseChangeMe \
     "http://localhost:9200/_cluster/health?wait_for_status=yellow" \
     && break
   sleep 5
 done
 
 curl -X POST -v -k "http://localhost:9200/_security/user/kibana/_password?pretty" \
-    -u elastic:   -H 'Content-Type: application/json' \
-    -d ' { "password" : "" } ' \
+    -u elastic:PleaseChangeMe   -H 'Content-Type: application/json' \
+    -d ' { "password" : "PleaseChangeMe" } ' \ 
     && echo "success change password success"
+    
   
